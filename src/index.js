a322c66388133f198614aeb3b7444270ab058b14
Your Name <you@example.com>
2017-07-05 23:50:47 +0200

    a

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const App = () => {
+  return <div> Stuff </div>;
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
