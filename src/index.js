21337741982a4933505bcf232afc125e989bb559
Your Name <you@example.com>
2017-07-06 20:00:19 +0200

    2

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyDWoeIyBsle7HmK_gwoT5N3fUcOYCfefkc';
 
 const App = () => {
-  return <div> Stuff </div>;
+  return
+
+  <div>
+    <SearchBar />
+  </div>;
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
