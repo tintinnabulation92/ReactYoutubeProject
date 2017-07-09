25036c37b6745e886a0f151014308176900ca4a7
Your Name <you@example.com>
2017-07-09 23:47:24 +0200

    11;47 niedziela

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyDWoeIyBsle7HmK_gwoT5N3fUcOYCfefkc';
 
@@ -24,7 +25,9 @@ constructor(props){
     return (
       <div>
         <SearchBar />
+        <VideoDetail video = {this.state.videos[2]} />
         <VideoList videos = {this.state.videos} />
+
       </div>
     );
   }
